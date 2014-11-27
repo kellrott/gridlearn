ed4f4f1e5f0b16095628d882577e4ae6fbf0802a
Kyle Ellrott <kellrott@gmail.com>
2014-11-27 13:50:18 -0800

    Changing to mapPartition based calculations, to avoid reloading data.

--- grid_learn.py
+++ grid_learn.py
@@ -11,37 +11,63 @@ from sklearn import cross_validation
 from sklearn import metrics
 
 
-def get_count(path, label):
-    matrix = pandas.read_csv(path, sep="\t", index_col=0)
-    return (label, sum( matrix[label] != 0 ))
 
-def learn_label(label_prefix, label_path, feature_path, label, fold=None, fold_count=None, transpose=False, penalty="l2", C=1.0):
-    observation_matrix = pandas.read_csv(feature_path, sep="\t", index_col=0).fillna(0.0)
+def learn_partition(partition, transpose, fold_count, penalty, C):
+    #rdd format: label_prefix, label, label_file_path, feature_file_path, fold_number
+    cur_label_path = None
+    cur_label_matrix = None
+    cur_feature_path = None
+    cur_feature_matrix = None
+
+    for label_prefix, label, label_file_path, feature_file_path, fold_number in partition:
+        if label_file_path != cur_label_path:
+            cur_label_matrix = pandas.read_csv(label_file_path, sep="\t", index_col=0)
+            if transpose:
+                cur_label_matrix = cur_label_matrix.transpose()
+            cur_label_path = label_file_path
+        if feature_file_path != cur_feature_path:
+            cur_feature_matrix = pandas.read_csv(feature_file_path, sep="\t", index_col=0)
+            if transpose:
+                cur_feature_matrix = cur_feature_matrix.transpose()
+            cur_feature_path = feature_file_path
+        yield learn_label(label_prefix=label_prefix, label_matrix=cur_label_matrix,
+            feature_matrix=cur_feature_matrix, label=label, fold=fold_number,
+            fold_count=fold_count, penalty=penalty, C=C)
+
+
+def learn_label_path(label_prefix, label_path, feature_path, label, fold=None, fold_count=None, transpose=False, penalty="l2", C=1.0):
+    feature_matrix = pandas.read_csv(feature_path, sep="\t", index_col=0).fillna(0.0)
     label_matrix = pandas.read_csv(label_path, sep="\t", index_col=0)
 
-    print "Learning", label, fold
-
     if transpose:
-        observation_matrix = observation_matrix.transpose()
+        feature_matrix = feature_matrix.transpose()
         label_matrix = label_matrix.transpose()
 
-    isect = observation_matrix.index.intersection(label_matrix.index)
+    return learn_label(label_prefix=label_prefix,
+        label_matrix=label_matrix,
+        feature_matrix=feature_matrix, label=label,
+        fold=fold, fold_count=fold_count, penalty=penalty, C=C)
+
+
+def learn_label(label_prefix, label_matrix, feature_matrix, label, fold=None, fold_count=None, penalty="l2", C=1.0):
+
+    isect = feature_matrix.index.intersection(label_matrix.index)
 
     labels = pandas.DataFrame(label_matrix[label]).reindex(isect)
-    observations = observation_matrix.reindex(isect)
+    features = feature_matrix.reindex(isect)
 
     if fold is None or fold_count is None:
         train_label_set = numpy.ravel(labels)
-        train_obs_set = observations
+        train_obs_set = features
         test_label_set = train_label_set
         test_obs_set = train_obs_set
     else:
         kf = cross_validation.KFold(len(isect), n_folds=fold_count, shuffle=True, random_state=42)
         train_idx, test_idx = list(kf)[fold]
         train_label_set = numpy.ravel(labels.iloc[train_idx])
-        train_obs_set = observations.iloc[train_idx]
+        train_obs_set = features.iloc[train_idx]
         test_label_set = numpy.ravel(labels.iloc[test_idx])
-        test_obs_set = observations.iloc[test_idx]
+        test_obs_set = features.iloc[test_idx]
 
     train_pos_label_count = sum(numpy.ravel(train_label_set != 0))
     test_pos_label_count = sum(numpy.ravel(test_label_set != 0))
@@ -71,13 +97,13 @@ def learn_label(label_prefix, label_path, feature_path, label, fold=None, fold_c
         pred=lr.predict_proba( test_obs_set )
         fpr, tpr, thresholds = metrics.roc_curve(test_label_set, list( a[1] for a in pred ))
         roc_auc = metrics.auc(fpr, tpr)
-        
+
         predictions = zip( test_label_set, list( a[1] for a in pred ) )
 
         prec, recall, thresholds = metrics.precision_recall_curve(test_label_set, list( a[1] for a in pred ))
         pr_auc = metrics.auc(prec, recall, reorder=True)
 
-        coef = dict(list(a for a in zip(observations.columns, lr.coef_[0]) if a[1] != 0 ))
+        coef = dict(list(a for a in zip(features.columns, lr.coef_[0]) if a[1] != 0 ))
 
         non_zero = sum( list( i != 0.0 for i in lr.coef_[0]) )
         rval['roc_auc'] = roc_auc
@@ -129,7 +155,6 @@ if __name__ == "__main__":
             feature_matrix = pandas.read_csv(feature_path, sep="\t", index_col=0)
             if args.transpose:
                 feature_matrix = feature_matrix.transpose()
-
             sample_intersect = label_matrix.index.intersection(feature_matrix.index)
             if len(sample_intersect) > 5:
                 label_set = []
@@ -141,7 +166,7 @@ if __name__ == "__main__":
         for label_prefix, label, label_file_path, feature_file_path in grid:
             if label == args.single:
                 print json.dumps(
-                    learn_label(
+                    learn_label_path(
                         label_path=label_file_path, feature_path=feature_file_path, label=args.single,
                         fold=0, fold_count=args.folds, transpose=args.transpose, label_prefix=label_prefix,
                         penalty=args.penalty, C=args.C )
@@ -165,10 +190,10 @@ if __name__ == "__main__":
             task_rdd = label_rdd.map( lambda x: x + [None] )
 
         #rdd format: label_prefix, label, label_file_path, feature_file_path, fold_number
-        counts = task_rdd.map(
-            lambda x: learn_label(
-                label_path=x[2], feature_path=x[3], label=x[1], fold=x[4],
-                fold_count=args.folds, transpose=args.transpose, label_prefix=x[0],
+        counts = task_rdd.mapPartitions(
+            lambda x: learn_partition(
+                partition=x,
+                fold_count=args.folds, transpose=args.transpose,
                 penalty=args.penalty, C=args.C)
             )
 
